b6cd52b793e073c1a7254e18f6a38f05b80d7273
enniob <enniobozzetti@gmail.com>
2021-09-02 15:08:33 -0400

    2 files modified

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import {
+  AbstractControl,
+  FormGroup,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -9,17 +14,34 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 export class ContactFormComponent implements OnInit {
   contactForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.contactForm = this.fb.group({
       name: this.fb.control('', Validators.compose([Validators.required])),
       email: this.fb.control('', Validators.compose([Validators.required])),
+      phoneNumber: this.fb.control(
+        '',
+        Validators.compose([Validators.required])
+      ),
       message: this.fb.control('', Validators.compose([Validators.required]))
     });
   }
 
   save() {
     console.log(this.contactForm);
+  }
+
+  get phoneNumber() {
+    return this.contactForm.get('phoneNumber');
+  }
+
+  private phoneNumberValidator(
+    control: AbstractControl
+  ): { [key: string]: any } | null {
+    const valid = /^\d+$/.test(control.value);
+    return valid
+      ? null
+      : { invalidNumber: { valid: false, value: control.value } };
   }
 }
